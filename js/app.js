14d616ed47099c9a08fba3bb9e81c41c51536c8f
Nuttdguy <pygnasak@gmail.com>
2015-12-14 01:29:37 -0600

    Add jQuery and append function. Correct class labels for input tag of purchased table.

--- js/app.js
+++ js/app.js
@@ -1,17 +1,30 @@
 $(document).ready(function() {
 
-	var itemDescription, itemPrice;
+//	var itemDescription, itemPrice;
 	
 	$('button').on('click', function() {
-		$('#add-item-form').serialize(function() {
-			var x = 
-			console.log('this is working')
-			itemDescription = $('.add-item-name').text();
-			itemPrice = $('.add-item-price').text();
-			console.log(itemDescription + ' ' + itemPrice);
-			
-		})
+		console.log('this is working');
+		var itemDescription = $('.add-item-name').val();
+		var itemPrice = $('.add-item-price').val();
 		
+		console.log(itemDescription);
+		
+		var inputQty = $('<input type="text" placeholder="1" value="1" maxlength="2" />');
+		var inputDescription = $('<input class="td-item-description-input" type="text" disabled="true">').text($('input[type=text].add-item-name')).val(itemDescription);
+		var inputPrice = $('<input class="td-item-price-input" type="text" disabled="true">').text($('input[type=text].add-item-price')).val(itemPrice);
+		var inputTotal = $('<input class="td-item-total-input" type="text" disabled="true">').text($('input[type=text].add-item-price')).val(itemPrice);
+		
+		var tr = $("<div>", {class: "tr"});
+		var tdQty = $("<div>", {class: "td td-item-qty"}).append(inputQty);
+		var tdDescription = $("<div>", {class: "td td-item-description"}).append(inputDescription);
+		var tdPrice = $("<div>", {class: "td td-item-price"}).append(inputPrice);
+		var tdTotal = $("<div>", {class: "td td-item-total"}).append(inputTotal);
+		
+		var tableRow = $(tr).append(tdQty).append(tdDescription).append(tdPrice).append(tdTotal).appendTo('.tbody');
+
+		var clearNameInput = $('.add-item-name, :reset').val('');
+		var clearPriceInput = $('.add-item-price, :reset').val('');	
 	});
 	
 });
+
