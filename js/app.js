e7870afa4cb950119c1f0a36a4c09fd1de67d61c
Nuttdguy <pygnasak@gmail.com>
2015-12-14 23:10:07 -0600

    Add Draggable and Droppable. Enable trash feature.

--- js/app.js
+++ js/app.js
@@ -1,8 +1,9 @@
 $(document).ready(function() {
 	
 	sort();
+	trash();
 	addItems();
-
+	
 });
 
 //  SORT LIST ITEMS BETWEEN TABLES
@@ -12,7 +13,33 @@ function sort() {
 		connectWith: ".connectedSortable"
 	}).disableSelection();
 };
+
+//function sort() {
+//	
+//	$( "div.droptrue" ).sortable({
+//		connectWith: ".connectedSortable"
+//	});
+//
+//	$( "div.dropfalse" ).sortable({
+//		connectWith: ".connectedSortable",
+//		dropOnEmpty: false
+//	});
+//
+//	$( "#sortable1, #sortable2, #sortable3" ).disableSelection();
+//	
+//});
 	
+	
+function trash() {
+	
+	$('.ui-state-default').draggable();
+		
+	$('.total-all').droppable({
+		drop: function(event, ui) {
+			$(ui.draggable).remove();
+		}
+	});
+};
 
 //  ADD ITEMS TO SHOPPING LIST
 
@@ -22,21 +49,20 @@ function addItems() {
 
 		var itemDescription = $('.add-item-name').val();
 		var itemPrice = $('.add-item-price').val();
-		
+		var uniqueTime = new Date().getTime();
+			
 		var inputQty = $('<input class="col-qty-input ui-state-default" type="text" placeholder="1" maxlength="2" >');
 		var inputDescription = $('<input class="td-item-description-input ui-state-default" type="text" placeholder="Shopping list item" >').text($('input[type=text].add-item-name')).val(itemDescription);
-		var inputPrice = $('<input class="td-item-price-input ui-state-default" type="text" placeholder="$ 0.00"  disabled >').text($('input[type=text].add-item-price')).val(itemPrice);
-		var inputTotal = $('<input class="td-item-total-input ui-state-default" type="text" placeholder="$ 0.00"  disabled >').text($('input[type=text].add-item-price')).val(itemPrice);
-		var trash = $('<img class="trash ui-state-default" src="img/x.png" alt="trash">');
+		var inputPrice = $('<input class="td-item-price-input ui-state-default" type="number" placeholder="$ 0.00" name="quantity" disabled >').text($('input[type=number].add-item-price')).val(itemPrice);
+		var inputTotal = $('<input class="td-item-total-input ui-state-default" type="number" placeholder="$ 0.00" name="quantity" disabled >').text($('input[type=number].add-item-price')).val(itemPrice);
 		
-		var ul = $("<ul>", {class: "tr"});
+		var ul = $("<ul>", {class: "tr", id: uniqueTime});
 		var li = $("<li>", {class: "td" });
 		var listItems = $(li).append(function() {
 			$(this).append(inputQty);
 			$(this).append(inputDescription);
 			$(this).append(inputPrice);
 			$(this).append(inputTotal);
-			$(this).append(trash);
 		})
 		
 		var tableRow = $(ul).append(listItems).appendTo('.tbody-shopping');
@@ -45,8 +71,7 @@ function addItems() {
 		var clearPriceInput = $('.add-item-price, :reset').val('$ ');	
 	});
 	
-	
-}
+};
 	
 	
 	
